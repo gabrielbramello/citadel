4f72a0386484de3b232cd5ad3417453b9842fee8
Gabriel Mello <gabrielbramello@gmail.com>
2020-07-25 14:07:39 -0300

    Modificação caminho cert

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,2 +1,4 @@
 require 'openid/fetchers'
-OpenID.fetcher.ca_file = '/etc/ssl/certs/ca-certificates.crt'
+##Versão antiga
+##OpenID.fetcher.ca_file = '/etc/ssl/certs/ca-certificates.crt'
+OpenID.fetcher.ca_file = '/etc/ssl/certs/ca-bundle.crt'
